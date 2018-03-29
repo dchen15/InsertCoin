158743ec0a4513e535022d2abc09c2af680b8dd4
Daniel <chen.daniel.97@gmail.com>
2018-03-29 00:52:45 -0400

    Creates angular js files for test pages

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-var insertCoin = angular.module('insertCoin', []);
+var app = angular.module('Insert Coin', ['ngRoute']);
 
-insertCoin.controller('mainController', function($scope) {
-$scope.message = 'Insert Coin';
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+
+        .when('/Bitcoin', {
+            templateUrl: "pages/Bitcoin.html",
+            controller: "BitcoinController"
+        })
+        .when('/Litecoin', {
+            templateUrl: "pages/Litecoin.html",
+            controller: "LitecoinController"
+        })
+        .when('/Burstcoin', {
+            templateUrl: "pages/Burstcoin.html",
+            controller: "BurstcoinController"
+        })
+        .otherwise({
+            redirectTo: "/Bitcoin"
+        });
+});
+
+app.controller('BitcoinController', function ($scope) {
+    $scope.message = "Angular Bitcoin";
+});
+
+app.controller('LitecoinController', function ($scope) {
+    $scope.message = "Litecoin";
+});
+
+app.controller('BurstcoinController', function ($scope) {
+    $scope.message = "Burstcoin";
 });
