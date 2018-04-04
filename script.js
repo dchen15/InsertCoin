83a46dcd78b3238aa00fe30203e2011effc649a6
Daniel <chen.daniel.97@gmail.com>
2018-04-04 02:00:49 -0400

    adds Daily information to btc page

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ app.controller('BitcoinController', function ($scope) {
     $scope.cell1 = "btcBuying";
     $scope.cell2 = "btcSelling";
     $scope.cell3 = "btcVolume";
+    $scope.cell4 = "btcDaily";
+    $scope.cell5 = "btcHigh";
+    $scope.cell6 = "btcLow";
 });
 
 app.controller('LitecoinController', function ($scope) {
@@ -62,6 +65,10 @@ BTCUSD.onmessage = function (msg) {
         document.getElementById("btcBuying").innerHTML = "Buying Price: $" + response[3];
         document.getElementById("btcSelling").innerHTML = "Selling Price: $" + response[1];
         document.getElementById("btcVolume").innerHTML = "Volume: $" + response[8];
+        document.getElementById("btcDaily").innerHTML = "Daily Change: $" + response[5];
+        document.getElementById("btcHigh").innerHTML = "Daily High: $" + response[9];
+        document.getElementById("btcLow").innerHTML =
+            "Daily Low: $" + response[10];
     };
 };
 
