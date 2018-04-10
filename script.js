8ef99c0959993bf277566c08f68443b633c0ff10
Daniel <chen.daniel.97@gmail.com>
2018-04-10 03:52:33 -0400

    adds sidebar when screen size gets smaller than 800px

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 var app = angular.module('Insert Coin', ['ngRoute']);
 
 app.config(function ($routeProvider, $locationProvider) {
