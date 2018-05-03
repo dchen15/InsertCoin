05593163939f7fa20bc5ac9b39c705cf0a2395b7
Ian Jaffe <ijaffe@albany.edu>
2018-05-03 18:21:18 -0400

    Adds a second graph

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ app.config(function ($routeProvider, $locationProvider) {
 
 app.controller('BitcoinController', function ($scope) {
     $scope.graph = "Bitcoingraph.html";
+	$scope.graph2 = "Bitcoingraph2.html";
     $scope.next = "#/Litecoin";
     $scope.previous = "#/Santiment";
     $scope.cell1 = "btcBuying";
